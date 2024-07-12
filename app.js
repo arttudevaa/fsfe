b15b51fa0670cf74654a694947292d4b7c31c4db
Arttu <arttukayra@gmail.com>
2024-07-12 14:23:30 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+	res.write("<h1>Testing testing</h1>");
+	res.end();
+}).listen(3000);
+
+console.log("server started on port 3000")
+
