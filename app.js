de85d23f0b1cbc566e770929f793ef74c08335d9
Arttu <arttukayra@gmail.com>
2024-07-13 23:28:32 +0300

    sqlite database

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-	res.write("<h1>Testing testing</h1>");
-	res.end();
+    res.write('<h1>Testing testing</h1>');
+    res.end();
 }).listen(3000);
 
-console.log("server started on port 3000")
-
+console.log('server started on port 3000');
